eb3442d77533e97ed5904849b00137f7472a76ff
Pedro Odake <pedroodake@gmail.com>
2025-08-27 14:33:53 -0300

    criado as views, rotas, template do app heroes

---
+++ heroes/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('hello/', views.hello_heroes, name='hello_heroes'),
+    path('lista/', views.lista_herois, name='lista_heroes'),
+    path('cbv-lista/', views.HeroListView.as_view(), name='cbv_lista_herois'),
+]
+
--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Hero
+from django.views.generic import ListView
 
-# Create your views here.
+def hello_heroes(request):
+    return HttpResponse("Bem-vindo ao módulo Heroes!")
+
+def lista_herois(request):
+    herois = Hero.objects.all()  # busca todos os heróis do banco
+    return render(request, "heroes/lista_herois.html", {"herois": herois})
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = "heroes/lista_herois.html"
+    context_object_name = "herois"
